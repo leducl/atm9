002384079d99b2ee64d65803ff324b0669c7d596
Leo <leducl@3il.fr>
2025-06-25 11:03:53 +0200

    Expose button handlers when using module scripts

---
+++ supabaseClient.js
@@ -0,0 +1,5 @@
+const supabase = window.supabase.createClient(
+  "https://glercnffjbmetbystnwi.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsZXJjbmZmamJtZXRieXN0bndpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA3NjgwNDQsImV4cCI6MjA2NjM0NDA0NH0.Z9JvWS5zNp8rqyAVkSEIiVpB63gxM5g8e8bL6u6C64w"
+);
+export default supabase;
